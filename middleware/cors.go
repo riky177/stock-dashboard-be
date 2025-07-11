282f459f1bc4f72e798bdef8f0b92424d8e99352
Riky Andreas <rikyandreas@Rikys-MacBook-Pro.local>
2025-07-11 12:20:19 +0700

    refactor: add frontend domain

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"https://stock-dashboard-fe.vercel.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
